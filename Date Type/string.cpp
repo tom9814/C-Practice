8a901360f701a5739da1acb54721b74c2ed6c767
Spider From Mars <assdavid985@gmail.com>
2026-09-04 11:40:18 +0800

    字符串类型的练习

---
+++ Date Type/string.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    char str1[] = "Fuck you";
+    cout << str1 << endl;
+    string  str2 = "Fuck you";
+    cout << str2 << endl;
+    return 0;
+}
