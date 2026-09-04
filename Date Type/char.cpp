f2507d49ca040a0ac4ccae29510f83e6aacd804b
Spider From Mars <assdavid985@gmail.com>
2026-09-04 11:16:11 +0800

    char类型的练习

---
+++ Date Type/char.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    char ch1 = 'a';
+    cout << ch1 << endl;
+    cout << "char占字节" << sizeof(char) << endl;
+    cout << (int)ch1 << endl;
+    return 0;
+}
