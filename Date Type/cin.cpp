0aa71004d3c75c506c29f0b742963229fce45c9e
Spider From Mars <assdavid985@gmail.com>
2026-09-05 14:40:50 +0800

    cin输入函数的练习

---
+++ Date Type/cin.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    int num = 1;
+    cout << "给整型num赋值" << endl;
+    cin >> num;
+    cout << "num = " << num << endl;
+    // string str = "Love you";
+    // cout << "给字符串str赋值" << endl;
+    // cin >> str;
+    // cout << "str = " << str << endl;
+    return 0;
+}
